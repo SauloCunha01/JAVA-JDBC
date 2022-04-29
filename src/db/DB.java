428006fbeb6063d36119169dcc2d6dc7ac423fbe
SauloCunha01 <saulocunha01@hotmail.com>
2022-04-29 09:29:07 -0300

    Conexão JDBC mysql

---
+++ src/db/DB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+import java.sql.*;
+/**
+ *
+ * @author Saulo
+ */
+public class DB {
+    
+    private static Connection conn = null;
+    
+    public static Connection getConncetion(){
+        if(conn !=null){
+           try{
+               conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/aluno", "root", "");
+           }catch(SQLException e){
+               throw  new DBException(e.getMessage());
+           }
+            
+        }
+        
+        return conn;
+    }
+    
+    
+    
+}
