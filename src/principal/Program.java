4cead26e0824ec36501de4eff5699bb3b9f7a847
SauloCunha01 <saulocunha01@hotmail.com>
2022-04-29 14:53:33 -0300

    Matendo a Integridade com, commit, rollback setAutoCommit

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -6,6 +6,7 @@
 package principal;
 
 import db.DB;
+import db.DBException;
 import db.DBIntegrityException;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -120,10 +121,51 @@ public class Program {
             DB.closeConnection();
         }
     }
+    
+    public static void updateComIntegridade(){
+        try {
+			conn  = DB.getConncetion();
+	
+			conn.setAutoCommit(false);
+
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+                        /*cauando uma excessão no meio da transação
+			int x = 1;
+                        if (x < 2) {
+			throw new SQLException("Fake error");
+			}
+			*/
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("rows1 = " + rows1);
+			System.out.println("rows2 = " + rows2);
+		}
+		catch (SQLException e) {
+			try {
+                            //caso ocorra algum erro de transação, vai voltar para a integridade inicial dos dados
+				conn.rollback();
+				throw new DBException("Transaction rolled back! Caused by: " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DBException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+    }
+    
     public static void main(String[] args) {
     
       
-        excluirDados();
+        //excluirDados();
+        updateComIntegridade();
     }
     
 }
